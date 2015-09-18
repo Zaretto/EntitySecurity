d6077bf3ad2026ae5e07011f5631d02207f56f90
Richard Harrison <rjh@zaretto.com>
2015-09-18 13:53:17 +0200

    2.0.1 - final part of interface rework this is to expose the original message text as a separate element in SystemStatusException (all of the other arguably less useful fields are already available)
    The field convention is to store the compound version in the Exception message as follows
    
    %FACILITY-SEVERITY-IDENT-MESSAGE TEXT
    
    and this makes the message text easily available.

--- ReferenceMonitor/Properties/AssemblyInfo.cs
+++ ReferenceMonitor/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5e5f6419-c3a0-49e0-a049-707ca97731b1")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
