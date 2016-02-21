f52a7651c35b5a8b4d2891b1dcb3db2bf4c0717a
Richard Harrison <rjh@zaretto.com>
2016-02-21 11:57:16 +0100

    Portable library;
    Added GitVersioning
    
    Reworked directories

--- ReferenceMonitor/Privilege.cs
+++ ReferenceMonitor/Privilege.cs
@@ -3,20 +3,86 @@
     /// <summary>
     /// Users can be assigned certain privileges that permit actions that would be protected.
     /// </summary>
+    /// <note>
+    /// The GROUPADMIN GROUPSYSTEM USERPRIV1 USERPRIV2 are to allow privileges to be added to the implementations and still retain
+    /// values defined here. It is also possible to extend beyond the values defined here using bits 16 onwards.
+    /// </note>
     public enum Privilege
     {
-        BYPASS = 1 << 1,    // Bypass object protection,                     All
-        DIAGNOSE = 1 << 2,    // Diagnose objects                            Objects
-        EXQUOTA = 1 << 3,    // May exceed quotas=                           Devour
-        GROUP = 1 << 4,    // Access via group protection when not in group  Group
-        IMPERSONATE = 1 << 5,    // Become another subject/user,             All
-        IMPORT = 1 << 6,    // Perform import operations,                    Objects
-        OPER = 1 << 7,    // Act as system operator,                         System
-        READALL = 1 << 8,    // Read any object bypassing,                   Objects
-        SECURITY = 1 << 9,    // Perform Security Operations,                System
-        SETPRV = 1 << 10,   // Change own privilege levels,                  All
-        SYSPRV = 1 << 11,   // Access objects via system protection,         All
-        GROUPADMIN = 1 << 12,   // No meaning internally - external marker   None
+        /// <summary>
+        /// Bypass object protection,                     All
+        /// </summary>
+        BYPASS = 1 << 1,
+
+        /// <summary>
+        /// Diagnose objects                            Objects
+        /// </summary>
+        DIAGNOSE = 1 << 2,
+
+        /// <summary>
+        /// May exceed quotas                           Devour
+        /// </summary>
+        EXQUOTA = 1 << 3,
+
+        /// <summary>
+        /// Access via group protection when not in group  Group
+        /// </summary>
+        GROUP = 1 << 4,
+
+        /// <summary>
+        /// Become another subject/user             All
+        /// </summary>
+        IMPERSONATE = 1 << 5,
+
+        /// <summary>
+        /// Perform import operations,                    Objects
+        /// </summary>
+        IMPORT = 1 << 6,
+
+        /// <summary>
+        /// Act as system operator,                         System
+        /// </summary>
+        OPER = 1 << 7,
+
+        /// <summary>
+        /// Read any object bypassing,                   Objects
+        /// </summary>
+        READALL = 1 << 8,
+
+        /// <summary>
+        /// Perform Security Operations,                System
+        /// </summary>
+        SECURITY = 1 << 9,
+
+        /// <summary>
+        /// Change own privilege levels,                  All
+        /// </summary>
+        SETPRV = 1 << 10,
+
+        /// <summary>
+        /// Access objects via system protection,         All
+        /// </summary>
+        SYSPRV = 1 << 11,
+
+        /// <summary>
+        /// No meaning internally - external marker   None
+        /// </summary>
+        GROUPADMIN = 1 << 12,
+
+        /// <summary>
+        /// No meaning internally - external marker   None
+        /// </summary>
+        GROUPSYSTEM = 1 << 13,
+
+        /// <summary>
+        /// No meaning internally - external marker   None
+        /// </summary>
+        USERPRIV1 = 1 << 14,
+
+        /// <summary>
+        /// No meaning internally - external marker   None
+        /// </summary>
+        USERPRIV2 = 1 << 15,
     };
 
     public class PrivilegeHelper
--- ReferenceMonitor/Properties/AssemblyInfo.cs
+++ ReferenceMonitor/Properties/AssemblyInfo.cs
@@ -15,14 +15,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ZarettoSecurity")]
 [assembly: AssemblyCopyright("Copyright ©  2015 Richard Hazrrison")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5e5f6419-c3a0-49e0-a049-707ca97731b1")]
-
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
